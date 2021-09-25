b801df21409be0bddb972cdb1dbcade163f479c5
NjakaRodstilere <rodstilere@gmail.com>
2021-09-25 17:25:10 +0300

    addition_fonctionnality

--- calc.js
+++ calc.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
+    var nb1=0;
+    var nb2=0;
+    var res=0;
+    $("#plus").click(function(){
+        nb1=parseInt($("#nb1").val());
+        nb2=parseInt($("#nb2").val());
+        if(nb1>50000 ){
+            $("#errornb1").text("nb1>50000");
+            $("#errornb1").css("display","block");
+            $("#errornb1").animate({opacity: 0.25,
+                left: "+=50",
+                height: "toggle"},2000,function(){
 
+            });
 
+        }else if(nb2>50000){
+            $("#errornb2").text("nb1>50000");
+            $("#errornb2").css("display","block");
+
+        }else{
+            res=nb1+nb2;
+            $("#result").val(res);
+        }
+        
+    });
 
 });
